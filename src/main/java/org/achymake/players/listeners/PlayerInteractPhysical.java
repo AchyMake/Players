eba53bec3155be0f41d340c1605dc72868382e01
AchyMake <achymake@gmail.com>
2024-01-17 20:10:30 +0100

    clean up

--- src/main/java/org/achymake/players/listeners/PlayerInteractPhysical.java
+++ src/main/java/org/achymake/players/listeners/PlayerInteractPhysical.java
@@ -2,6 +2,7 @@ package org.achymake.players.listeners;
 
 import org.achymake.players.Players;
 import org.achymake.players.files.Database;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -14,6 +15,9 @@ public class PlayerInteractPhysical implements Listener {
     private Database getDatabase() {
         return plugin.getDatabase();
     }
+    private FileConfiguration getConfig() {
+        return plugin.getConfig();
+    }
     public PlayerInteractPhysical(Players plugin) {
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
         this.plugin = plugin;
@@ -22,8 +26,11 @@ public class PlayerInteractPhysical implements Listener {
     public void onPlayerInteractPhysical(PlayerInteractEvent event) {
         if (!event.getAction().equals(Action.PHYSICAL))return;
         if (event.getClickedBlock() == null)return;
-        if (!isFrozenOrJailedOrVanished(event.getPlayer()))return;
-        event.setCancelled(true);
+        if (isFrozenOrJailedOrVanished(event.getPlayer())) {
+            event.setCancelled(true);
+        } else if (getConfig().getBoolean("disable-trample."+event.getClickedBlock().getType())) {
+            event.setCancelled(true);
+        }
     }
     private boolean isFrozenOrJailedOrVanished(Player player) {
         return getDatabase().isFrozen(player) || getDatabase().isJailed(player) || getDatabase().isVanished(player);
